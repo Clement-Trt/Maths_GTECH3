745b431056032970737d105f55a860e3c19e7a0c
Clement-Trt <ctouratier@gaming.tech>
2025-11-04 09:01:49 +0100

    Q) 1.2

--- Maths_GTECH3/Maths_GTECH3/Maths_GTECH3.cpp
+++ Maths_GTECH3/Maths_GTECH3/Maths_GTECH3.cpp
@@ -4,13 +4,40 @@
 #include <iostream>
 #include <windows.h>
 
+#include <thread>
+#include <chrono>
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    DWORD mode;
-    GetConsoleMode(hConsole, &mode);
-    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+
+    {   // Pas forcement necessaire, depend du compilateur
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        DWORD mode;
+        GetConsoleMode(hConsole, &mode);
+        SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    }
+
+
+    std::cout << "\x1b[2J";
+    std::cout << "bob\n";
+
+    std::cout << "\x1b[?25l";
+
+    std::cout << "Le curseur est cache pendant 3 secondes...\n";
+    //std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    // Réafficher le curseur
+    std::cout << "\x1b[?25h";
+    std::cout << "Curseur reactive.\n";
+
+    std::string a;
+    std::cin >> a;
+
+    std::cout << "\x1b[H\x1b[n";
+    std::cout << "bob2_ ";
+
     return 0;
 }
 
